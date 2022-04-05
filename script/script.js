ae234539d0e72480876cb7c2f03496f0ce9902f5
renanberton <renan_berton@yahoo.com.br>
2022-04-05 17:28:23 -0300

    Ordem + cores

--- script/script.js
+++ script/script.js
@@ -1,31 +1,25 @@
-for(var i = 1; i < 250; i++){
-fetch("https://pokeapi.co/api/v2/pokemon/" + i).then(resposta => {
-    return resposta.json()
-}).then(corpo => {
-
+for(var i = 1; i < 300; i++){
+    fetch("https://pokeapi.co/api/v2/pokemon/" + i).then(resposta => {
+        return resposta.json()
+    }).then(corpo => {        
         let section = document.querySelector('section');
-
+        
         let pokemon = document.createElement('div');
         pokemon.classList.add('pokemon');
+        pokemon.style=`order: ${corpo.id}`;
         
         let pokemonName = document.createElement('p');
-        let pokemonParagrafo = document.createElement('p');
-        let pokemonParagrafo1 = document.createElement('p');
         let pokemonSpan = document.createElement('span');
         let pokemonImg = document.createElement('img');
-
-        pokemonName.innerHTML = corpo.name.toUpperCase();
-
-        pokemonParagrafo.innerHTML = "Altura: " + corpo.height;
-        pokemonParagrafo1.innerHTML = corpo.id;
-        pokemonSpan.innerHTML = "Peso: " + corpo.weight;
-        pokemonImg.setAttribute('src', corpo.sprites.front_default);
-
-        pokemon.appendChild(pokemonName);
-        pokemon.appendChild(pokemonParagrafo);
-        pokemon.appendChild(pokemonParagrafo1);        
-        pokemon.appendChild(pokemonSpan);
-        pokemon.appendChild(pokemonImg);
+        
         section.appendChild(pokemon);        
+        pokemonName.innerHTML = corpo.name.toUpperCase();
+        
+        pokemonSpan.innerHTML = corpo.types[0].type.name.toUpperCase();
+        pokemonImg.setAttribute('src', corpo.sprites.front_default);
+        
+        pokemon.appendChild(pokemonName);
+        pokemon.appendChild(pokemonImg);
+        pokemon.appendChild(pokemonSpan);
     })
 }
